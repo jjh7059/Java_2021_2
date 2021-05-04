f04be7b5ba4831570e842c764908b84213e2596c
jjh7059 <wlswngus2@naver.com>
2021-05-05 07:03:08 +0900

    Overload review, added context

--- src/oop/Overload.java
+++ src/oop/Overload.java
@@ -16,6 +16,7 @@ public class Overload {
 	
 	//메소드 오버로드(Method Overload) 선언 : 동일한 기능을 제공하는 메소드가 매개변수에
 	//의해 여러 개 선언해야 될 경우 메소드명을 같게 선언하는 방법
+	//클래스에서 메소드 파라미터 갯수, 자료형등을 다르게 선언
 	public void display(int param) {
 		System.out.println("정수값 = " + param);
 	}
