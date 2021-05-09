dcb85de76e8a70a1d2d6b896d9f759f0421bda13
jjh7059 <wlswngus2@naver.com>
2021-05-09 20:15:37 +0900

    carapp annotation edit...

--- src/association/CarApp.java
+++ src/association/CarApp.java
@@ -32,7 +32,7 @@ public class CarApp {
 		System.out.println("=======================================================");
 		System.out.println(carTwo.getModelName() + "의 엔진 정보 >> ");
 		//carTwo.getCarEngine() : 참조필드에 저장된 Engine 인스턴스 반환
-		// => 반환받은 포함 관계의 인스턴스를 이용하여 메소드 호출 
+		// => 반환받은 포함 관계의 인스턴스를 이용하여 메소드를 호출
 		carTwo.getCarEngine().engineDisplay();
 		System.out.println("=======================================================");
 	}
